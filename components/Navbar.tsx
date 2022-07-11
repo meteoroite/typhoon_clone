3823ade8065828b83e00346b980266259d0733b1
meteoroite <90157302+meteoroite@users.noreply.github.com>
2022-07-11 16:06:05 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py px-4'>
       <Link href='/'>
-        <div className='w-[100px] md:w-[120px]  md:h-[120px] h-[38px]'>
+        <div className='w-[100px] md:w-[129px]  md:h-[30px] h-[38px]'>
           <Image
             className='cursor-pointer'
             src={Logo}
